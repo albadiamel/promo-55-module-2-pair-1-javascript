004a75774009069951ab4d87342919365cacad34
Alba Díaz Melián <albadiazgraficaimpresa@gmail.com>
2025-07-21 19:27:52 +0100

    ejercicios-bucles

--- main.js
+++ main.js
@@ -534,3 +534,80 @@ const buttonClick = document.querySelector(".buttonClick").addEventListener("cli
   console.log("Hiciste click") /*el function () console.log("Hiciste click") sería la función manejadora*/
 });
 
+
+// EJERCICIOS DE BUCLES
+// 1. Practicando
+// Crea un bucle que muestre 10 veces, en la consola, el texto "Voy por la vuelta X" siendo el número de vueltas desde 1 hasta 10 (no desde 0 hasta 9).
+
+for (let i = 0; i < 10; i++) {
+  console.log("Voy por la vuelta " + (i + 1));
+}
+
+//Vamos a partir de una variable acc con valor 0. Construiremos un bucle que se ejecute 10 veces y sume 2 a la variable acc en cada iteración del bucle. 
+// Al acabar el bucle, mostraremos en la consola el texto El resultado es: X, siendo X el valor de la variable acc.
+
+let acc = 0;
+
+for (let i = 0; i < 10; i++) {
+  acc = acc + 2;
+}
+
+console.log("El resultado es: " + acc);
+
+
+// 2. La media
+// a) Crea un nuevo array numbers que contendrá 5 números cualesquiera y recorre el array mediante un bucle para calcular la media de los números (la suma de los números dividido por cuántos hay, es decir, 5). 
+// Necesitas una variable (acumulador) para ir almacenando la suma de todos los números y después poder hacer la media. 
+// Para comprobar si el resultado es correcto, loguealo en la consola.
+
+const numbers  = [1, 2, 3, 4, 5, 6];
+
+let suma = 0;
+
+
+for (let i = 0; i < numbers.length; i++) {
+  suma = suma + numbers[i];
+}
+
+const mediaResult = suma / numbers.length;
+
+console.log("La media es " + mediaResult);
+
+// b) Añade un nuevo número al array y repetir el cálculo de la media. 
+// En este caso, para calcular la media habrá que sumar todos y dividir entre el total, que ahora es 6.
+
+// c) Crea una función average, que toma como parámetro un array numbers, calcula la media del array (de cualquier longitud) 
+// y devuelve la media. Para trabajar con arrays de cualquier longitud, deberemos consultar la longitud del array mediante su propiedad length. 
+// Para comprobar que la función hace el cálculo correcto, la invocaremos (o ejecutaremos para que no suene tan esotérico) varias veces 
+// pasándole como argumento un array con diferente longitud cada vez y mostraremos el resultado en la consola del navegador.
+
+function average (numbers) {
+  let suma = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    suma = suma + numbers[i];
+  }
+  const media = suma / numbers.length;
+  return media;
+}
+
+console.log("Media de [1, 2, 3, 4, 5]: " + average([1, 2, 3, 4, 5]));
+
+// 3. Tenemos mucho en común
+
+const inputOne = document.querySelector(".input--one");
+const inputTwo = document.querySelector(".input--two");
+const buttonBook = document.querySelector(".button--book");
+const paragraphBook = document.querySelector(".paragraph--book");
+
+const books = [];
+const handleClick = () => {
+  console.log("has hecho click");
+  const resultOne = inputOne.value;
+  const resultTwo = inputTwo.value;
+  books.push(resultOne, resultTwo);
+  for (const book of books) {
+    paragraphBook.innerHTML += " A mí también me encantó " + book;
+  }
+};
+
+buttonBook.addEventListener("click", handleClick);
